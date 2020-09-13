a78c307de2d674558393afa139b983f475a868a7
Juan Cobian <cobianjuan19@gmail.com>
2020-09-13 12:09:43 -0700

    create, show, update, and delete line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,17 +2,48 @@ class LineItemsController < ApplicationController
 
   def index
     line_items = LineItem.all
-    options = {
-        include: [:product]
-    }
-    render json: LineItemSerializer.new(line_items)
+    render json: line_items, include: [:cart, :product]
+  end
+
+  def create
+    @line_item = LineItem.new(line_items_params)
+    if @line_item.save
+      render json: {
+          status: :created,
+          line_item: @line_item, include: [:product, :cart]
+      }
+    end
   end
 
   def show
-    line_item = LineItem.find_by_id(params[:id])
-    options = {
-        include: [:product]
-    }
-    render json: LineItemSerializer.new(line_item)
+    @line_item = LineItem.find_by_id(params[:id])
+    if @line_item
+      render json: @line_item, include: [:product, :cart]
+    end
+  end
+
+  def update
+    @line_item = LineItem.find_by_id(params[:id])
+    if @line_item
+      @line_item.update(line_items_params)
+      @line_item.save
+      render json: @line_item, include: [:product, :cart]
+    end
+  end
+
+  def destroy
+    @line_item = LineItem.find_by_id(params[:id])
+    if @line_item
+      @line_item.destroy
+      render json: {
+          status: :deleted
+      }
+    end
+  end
+
+  private
+
+  def line_items_params
+    params.require(:line_item).permit(:product_id, :cart_id, :quantity, :total)
   end
 end
